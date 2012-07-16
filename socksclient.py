c956ab93483f0c5ca78734204043446e232ad20a
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2012-07-16 11:59:04 +0200

    SOCKSv5 implemented and TOR HS SOCKv5 errors handled

--- socksclient.py
+++ socksclient.py
@@ -39,66 +39,84 @@ from twisted.internet.interfaces import IStreamClientEndpoint
 from twisted.internet.protocol import Protocol, ClientFactory
 from twisted.internet.endpoints import _WrappingFactory
 
-class SOCKSError(Exception):
-    def __init__(self, val):
-        self.val = val
-    def __str__(self):
-        return repr(self.val)
+SOCKS_errors = {\
+    0x00: "request granted",
+    0x01: "general failure",
+    0x02: "connection not allowed by ruleset",
+    0x03: "network unreachable",
+    0x04: "host unreachable",
+    0x05: "connection refused by destination host",
+    0x06: "TTL expired",
+    0x07: "command not supported / protocol error",
+    0x08: "address type not supported",
+    0x23: "hidden service not found",
+    0x24: "hidden service not reachable"
+}
 
-class SOCKSv4ClientProtocol(Protocol):
+class SOCKSError(Exception):
+    def __init__(self, code, error):
+        self.code = code
+        self.error = error
+    def __str__(self):
+        return repr(self.error) + " (" + repr(self.code) + ")"
+
+class SOCKSv5ClientProtocol(Protocol):
     postHandshakeEndpoint = None
     postHandshakeFactory = None
     handshakeDone = None
     buf = ''
+    state = 0
 
-    def SOCKSConnect(self, host, port):
-        ver = 4 # only socksv4a for now
-        cmd = 1 # stream connection
-        user = '\x00'
-        dnsname = ''
-        try:
-            addr = socket.inet_aton(host)
-        except socket.error:
-            addr = '\x00\x00\x00\x01'
-            dnsname = '%s\x00' % host
-        msg = struct.pack('!BBH', ver, cmd, port) + addr + user + dnsname
-        self.transport.write(msg)
+    def socks_state_0(self, data):
+        # in SOCKSv5 protocol is always the client that send the first message
+        self.transport.loseConnection()
 
-    def verifySocksReply(self, data):
-        """
-        Return True on success, False on need-more-data.
-        Raise SOCKSError on request rejected or failed.
-        """
-        if len(data) < 8:
-            return False
-        if ord(data[0]) != 0:
+    def socks_state_1(self, data):
+        if data != "\x05\x00":
             self.transport.loseConnection()
-            raise SOCKSError((1, "bad data"))
-        status = ord(data[1])
-        if status != 0x5a:
-            self.transport.loseConnection()
-            raise SOCKSError((status, "request not granted: %d" % status))
-        return True
 
-    def isSuccess(self, data):
-        self.buf += data
-        return self.verifySocksReply(self.buf)
+            return
+
+        host = self.postHandshakeEndpoint._host
+        port = self.postHandshakeEndpoint._port
+            
+        # Anonymous access allowed - let's issue connect
+        self.transport.write(struct.pack("!BBBBB", 5, 1, 0, 3, len(host)) + host + struct.pack("!H", port))
+        
+        self.state = 2
+
+    def socks_state_2(self, data):
+        if data[:2] != "\x05\x00":
+            self.transport.loseConnection()
+
+            errcode = ord(data[1])
+            
+            if errcode in SOCKS_errors:
+                self.handshakeDone.errback(SOCKSError(hex(errcode), SOCKS_errors[errcode]))
+            else:
+                self.handshakeDone.errback(SOCKSError(hex(errcode), "unrecognized Error"))
+                
+            return
+
+        self.transport.protocol = self.postHandshakeFactory.buildProtocol(self.transport.getHost())
+        self.transport.protocol.transport = self.transport
+        self.transport.protocol.connectionMade()
+        #self.handshakeDone.callback("aaa")
+
+    def thisMustNeverHappen(self, data):
+        self.transport.loseConnection()
 
     def connectionMade(self):
-        self.SOCKSConnect(self.postHandshakeEndpoint._host,
-                          self.postHandshakeEndpoint._port)
-
+        # We implement only Anonymous access
+        self.transport.write(struct.pack("!BB", 5, len("\x00")) + "\x00")
+        
+        self.state = 1
+        
     def dataReceived(self, data):
-        if self.isSuccess(data):
-            # Build protocol from provided factory and transfer control to it.
-            self.transport.protocol = self.postHandshakeFactory.buildProtocol(self.transport.getHost())
-            self.transport.protocol.transport = self.transport
-            self.transport.protocol.connectionMade()
-            self.handshakeDone.callback(self.transport.getPeer())
+        getattr(self, 'socks_state_%s' % (self.state), self.thisMustNeverHappen)(data)
 
-
-class SOCKSv4ClientFactory(ClientFactory):
-    protocol = SOCKSv4ClientProtocol
+class SOCKSv5ClientFactory(ClientFactory):
+    protocol = SOCKSv5ClientProtocol
 
     def buildProtocol(self, addr):
         r = ClientFactory.buildProtocol(self, addr)
@@ -106,37 +124,41 @@ class SOCKSv4ClientFactory(ClientFactory):
         r.postHandshakeFactory = self.postHandshakeFactory
         r.handshakeDone = self.handshakeDone
         return r
-
+        
+    def clientConnectionFailed(self, connector, reason):
+        self.handshakeDone.errback("connection to sock server failed")
 
 class SOCKSWrapper(object):
     implements(IStreamClientEndpoint)
-    factory = SOCKSv4ClientFactory
+    factory = SOCKSv5ClientFactory
 
     def __init__(self, reactor, host, port, endpoint):
+        self._reactor = reactor
         self._host = host
         self._port = port
-        self._reactor = reactor
         self._endpoint = endpoint
-    
-    def _canceller(deferred):
-            connector.stopConnecting()
-            deferred.errback(error.ConnectingCancelledError(connector.getDestination()))
 
     def connect(self, protocolFactory):
         """
         Return a deferred firing when the SOCKS connection is established.
         """
 
+        def _canceller(deferred):
+            connector.stopConnecting()
+            deferred.errback(
+                error.ConnectingCancelledError(connector.getDestination()))
+
         try:
             # Connect with an intermediate SOCKS factory/protocol,
             # which then hands control to the provided protocolFactory
             # once a SOCKS connection has been established.
+
             f = self.factory()
             f.postHandshakeEndpoint = self._endpoint
             f.postHandshakeFactory = protocolFactory
             f.handshakeDone = defer.Deferred()
-            wf = _WrappingFactory(f, self._canceller)
-            self._reactor.connectTCP(self._host, self._port, wf)
+
+            connector = self._reactor.connectTCP(self._host, self._port, f)
             return f.handshakeDone
         except:
             return defer.fail()
