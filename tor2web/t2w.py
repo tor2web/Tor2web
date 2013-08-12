72dc7fee6633a06fc05600be4f65d8f41fef2b0e
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-08-13 01:04:59 +0200

    added antanistaticmap/dev/null api in order to support decoy traffic

--- tor2web/t2w.py
+++ tor2web/t2w.py
@@ -35,6 +35,7 @@ import os
 import re
 import sys
 import mimetypes
+import random
 import zlib
 import hashlib
 from StringIO import StringIO
@@ -646,7 +647,14 @@ class T2WRequest(http.Request):
         if resource_is_local:
             # the requested resource is local, we deliver it directly
             try:
-                if staticpath == "notification":
+                print staticpath
+                if staticpath == "dev/null":
+                    content = "A" * random.randint(20, 1024)
+                    defer.returnValue(self.contentFinish(content))
+                    print content
+                    return
+
+                elif staticpath == "notification":
  
                     #################################################################
                     # Here we need to parse POST data in x-www-form-urlencoded format
