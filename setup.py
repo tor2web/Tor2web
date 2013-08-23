e663a85a7595097215328ea7de2c9345f84a44a0
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-08-23 16:32:43 +0200

    Bump to version 3.0.27

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ data_files = [
 
 setup(
     name="tor2web",
-    version="3.0.26",
+    version="3.0.27",
     author="Random GlobaLeaks developers",
     author_email = "info@globaleaks.org",
     url="https://tor2web.org/",
