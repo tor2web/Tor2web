50d9322a32a59af9fa0b3869edccef86a1d4e986
Giovanni `evilaliv3` Pellerano <giovanni.pellerano@evilaliv3.org>
2013-11-28 10:17:36 +0100

    Bump to version 3.0.75

--- setup.py
+++ setup.py
@@ -10,40 +10,28 @@ def get_requires():
         requires = f.readlines()
         return requires
 
+def list_files(path):
+    result = []
+    for f in os.listdir(path):
+        result.append(os.path.join(path, f))
+
+    return result
+
 data_files = [
     ('/etc', [
     os.path.join('data', 'tor2web.conf.example'),
     ]),
-    ('/usr/share/tor2web/lists', [
-    os.path.join('data', 'lists', 'blocked_ua.txt'),
-    ]),
-    ('/usr/share/tor2web/static', [
-    os.path.join('data', 'static', 'robots.txt'),
-    os.path.join('data', 'static', 'index.html'),
-    os.path.join('data', 'static', 'tor2web.css'),
-    os.path.join('data', 'static', 'tor2web.js'),
-    os.path.join('data', 'static', 'tor2web.png'),
-    os.path.join('data', 'static', 'tor2web-big.png'),
-    os.path.join('data', 'static', 'tor2web-small.png'),
-    os.path.join('data', 'static', 'tos.html'),
-    os.path.join('data', 'static', 'decoy.html'),
-    ]),
-    ('/usr/share/tor2web/templates', [
-    os.path.join('data', 'templates', 'banner.tpl'),
-    os.path.join('data', 'templates', 'error_blocked_ua.tpl'),
-    os.path.join('data', 'templates', 'error_generic.tpl'),
-    os.path.join('data', 'templates', 'error_hs_completely_blocked.tpl'),
-    os.path.join('data', 'templates', 'error_hs_specific_page_blocked.tpl'),
-    os.path.join('data', 'templates', 'error_invalid_hostname.tpl'),
-    os.path.join('data', 'templates', 'error_sock_generic.tpl'),
-    os.path.join('data', 'templates', 'error_sock_hs_not_found.tpl'),
-    os.path.join('data', 'templates', 'error_sock_hs_not_reachable.tpl'),
-    ]),
+    ('/usr/share/tor2web/lists',
+    list_files(os.path.join('data', 'lists'))),
+    ('/usr/share/tor2web/static',
+    list_files(os.path.join('data', 'static'))),
+    ('/usr/share/tor2web/templates',
+    list_files(os.path.join('data', 'templates')))
 ]
 
 setup(
     name="tor2web",
-    version="3.0.74",
+    version="3.0.75",
     author="Random GlobaLeaks developers",
     author_email = "info@globaleaks.org",
     url="https://tor2web.org/",
