7ae1c2c47a6c098635e0284cf6d740445fbdf653
Giovanni `evilaliv3` Pellerano <giovanni.pellerano@evilaliv3.org>
2014-01-05 20:55:02 +0100

    implemented https://github.com/globaleaks/Tor2web-3.0/issues/127

--- tor2web/t2w-worker.py
+++ tor2web/t2w-worker.py
@@ -413,6 +413,9 @@ class T2WRequest(http.Request):
         self.setHeader(b'content-length', intToBytes(len(data)))
         self.setHeader(b'cache-control', b'no-cache')
 
+        if config['blockcrawl']:
+            self.setHeader(b'X-Robots-Tag', b'noindex')
+
         if self.isSecure():
             self.setHeader(b'strict-transport-security', b'max-age=31536000')
 
