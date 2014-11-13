tor2web/Tor2web

commit 8f1c1b504dc4df354ae703e0df603177d42af706

implemented https://github.com/globaleaks/Tor2web-3.0/issues/158

tor2web/t2w.py
@@ -68,6 +68,7 @@ from twisted.internet.task import LoopingCall
 from tor2web import __version__
 from tor2web.utils.config import Config
 from tor2web.utils.daemon import T2WDaemon, set_pdeathsig, set_proctitle
+from tor2web.utils.hostsmap import HostsMap
 from tor2web.utils.lists import List, TorExitNodeList
 from tor2web.utils.mail import sendmail, sendexceptionmail
 from tor2web.utils.misc import listenTCPonExistingFD, listenSSLonExistingFD, re_sub, verify_onion
@@ -89,6 +90,9 @@ class T2WRPCServer(pb.Root):
         self.stats = T2WStats()
         self.white_list = []
         self.black_list = []
+        self.TorExitNodes = []
+        self.blocked_ua = []
+        self.hosts_map = {}
 
         self.load_lists()
 
@@ -120,9 +124,13 @@ class T2WRPCServer(pb.Root):
                 self.blocked_ua.append(ua.lower())
 
         # Load Exit Nodes list with the refresh rate configured  in config file
-        self.TorExitNodes = TorExitNodeList(os.path.join(config.datadir, 'lists', 'exitnodelist.txt'),
-                                            "https://onionoo.torproject.org/summary?type=relay",
+        self.TorExitNodes = TorExitNodeList(config.t2w_file_path('lists/exitnodelist.txt'),
+                                            'https://onionoo.torproject.org/summary?type=relay',
                                             config.exit_node_list_refresh)
+
+        self.hosts_map = HostsMap(config.t2w_file_path('lists/hosts_map.txt')).hosts
+        if config.mode == "TRANSLATION":
+            self.hosts_map[config.basehost] = config.onion
 
     def remote_get_config(self):
         return self.config.__dict__
@@ -138,6 +146,9 @@ class T2WRPCServer(pb.Root):
 
     def remote_get_tor_exits_list(self):
         return list(self.TorExitNodes)
+
+    def remote_get_hosts_map(self):
+        return dict(self.hosts_map)
 
     def remote_update_stats(self, onion):
         self.stats.update(onion)
@@ -454,7 +465,7 @@ class T2WRequest(http.Request):
         data = self.stream + data
 
         if len(data) >= 1000:
-            data = re_sub(rexp['t2w'], r'https://\2.' + config.basehost, data)
+            data = re_sub(rexp['t2w_from'], rexp['t2w_to'], data)
 
             forward = data[:-500]
             if not self.header_injected and forward.find("<head") != -1:
@@ -476,7 +487,7 @@ class T2WRequest(http.Request):
 
         data = self.stream + data
 
-        data = re_sub(rexp['t2w'], r'https://\2.' + config.basehost, data)
+        data = re_sub(rexp['t2w_from'], rexp['t2w_to'], data)
 
         if not self.header_injected and data.find("<head") != -1:
             banner = yield flattenString(self, templates['banner.tpl'])
@@ -624,7 +635,7 @@ class T2WRequest(http.Request):
         for key, values in self.obj.headers.getAllRawHeaders():
             fixed_values = []
             for value in values:
-                value = re_sub(rexp['w2t'], r'http://\2.onion', value)
+                value = re_sub(rexp['w2t_from'], rexp['w2t_to'], value)
                 fixed_values.append(value)
 
             self.obj.headers.setRawHeaders(key, fixed_values)
@@ -793,14 +804,19 @@ class T2WRequest(http.Request):
 
             self.obj.uri = request.uri
 
-            if config.mode == "TRANSLATION":
-                self.obj.onion = config.onion
-            else:
-                self.obj.onion = request.host.split(".")[0] + ".onion"
-
             if not request.host:
                 self.sendError(406, 'error_invalid_hostname.tpl')
                 defer.returnValue(NOT_DONE_YET)
+
+            if config.mode == 'TRANSLATION' and request.host in hosts_map:
+                self.obj.onion = hosts_map[request.host]
+            else:
+                self.obj.onion = request.host.split(".")[0] + ".onion"
+
+            rexp['w2t_from'] = re.compile(r'(http.?:)?//' + request.host + '(?!:\d+)', re.I)
+            rexp['w2t_to'] = r'http://' + self.obj.onion
+            rexp['t2w_from'] = re.compile(r'(http.?:)?//([a-z0-9]{16}).onion(?!:\d+)', re.I)
+            rexp['t2w_to'] = r'https://' + request.host
 
             # we need to verify if the user is using tor;
             # on this condition it's better to redirect on the .onion
@@ -850,7 +866,7 @@ class T2WRequest(http.Request):
             # Avoid image hotlinking
             if config.blockhotlinking and request.uri.lower().endswith(tuple(config.blockhotlinking_exts)):
                 if request.headers.getRawHeaders(b'referer') is not None and \
-                   not config.basehost in request.headers.getRawHeaders(b'referer')[0].lower():
+                   not request.host in request.headers.getRawHeaders(b'referer')[0].lower():
                     self.sendError(403)
                     defer.returnValue(NOT_DONE_YET)
 
@@ -925,7 +941,7 @@ class T2WRequest(http.Request):
         if keyLower in 'location':
             fixed_values = []
             for value in values:
-                value = re_sub(rexp['t2w'], r'https://\2.' + config.basehost, value)
+                value = re_sub(rexp['t2w_from'], rexp['t2w_to'], value)
                 fixed_values.append(value)
             values = fixed_values
 
@@ -1084,9 +1100,7 @@ def start_worker():
     lc.start(600)
 
     rexp = {
-        'head': re.compile(r'(<head.*?\s*>)', re.I),
-        'w2t': re.compile(r'(http.?:)?//([a-z0-9]{16}).' + config.basehost + '(?!:\d+)', re.I),
-        't2w': re.compile(r'(http.?:)?//([a-z0-9]{16}).(?!' + config.basehost + ')onion(?!:\d+)', re.I)
+        'head': re.compile(r'(<head.*?\s*>)', re.I)
     }
 
     ###############################################################################
@@ -1206,12 +1220,17 @@ def updateListsTask():
         global tor_exits_list
         tor_exits_list = l
 
+    def set_hosts_map(d):
+        global hosts_map
+        hosts_map = d
+
     global config
 
     rpc("get_white_list").addCallback(set_white_list)
     rpc("get_black_list").addCallback(set_black_list)
     rpc("get_blocked_ua_list").addCallback(set_blocked_ua_list)
     rpc("get_tor_exits_list").addCallback(set_tor_exits_list)
+    rpc("get_hosts_map").addCallback(set_hosts_map)
 
 def SigQUIT(SIG, FRM):
     try:
@@ -1405,6 +1424,7 @@ else:
      black_list = []
      blocked_ua_list = []
      tor_exits_list = []
+     hosts_map = {}
      ports = []
 
      rpc_factory = pb.PBClientFactory()

data/static/tor2web.css
@@ -23,6 +23,7 @@
 #tor2web-visible { display: block; padding: 0 30px; }
 #tor2web-hidden { display: none; }
 #tor2web_logo { float: left; margin: 0 0 10px 10px; }
+#tor2web_disclaimer { float: left; }
 #tor2web_disclaimer_acceptance, #tor2web_terms, #tor2web_footer { margin-top: 30px; }
 #tor2web_notification_form { clear: both; display: none; clear: both; width: 600px; margin:0 auto; margin-bottom: 10px; }
 #tor2web_footer { text-align: center }

data/templates/banner.tpl
@@ -1,1 +1,42 @@
-<script xmlns:t="http://twistedmatrix.com/ns/twisted.web.template/0.1" type="text/javascript" src="/antanistaticmap/tor2web.js"></script>
+<div xmlns:t="http://twistedmatrix.com/ns/twisted.web.template/0.1">
+  <script type="text/javascript" src="/antanistaticmap/tor2web.js"></script>
+  <style type="text/css">
+  @import url(/antanistaticmap/tor2web.css);
+  </style>
+
+  <div id="tor2web-banner">
+    <div id="tor2web-visible">
+      <div id="tor2web_logo">
+        <a href="https://www.tor2web.org"><img src="/antanistaticmap/tor2web-small.png" alt="tor2web logo" /></a>
+      </div>
+      <div id="tor2web_disclaimer">
+        <div><b>tor2web.org does not host this content</b>; we are simply a conduit connecting Internet users to content hosted inside <a href="https://www.torproject.org/docs/hidden-services.html.en">the Tor network.</a></div>
+        <div>To obtain anonymity, you are strongly advised to <a href="https://www.torproject.org/download/">download the Tor Browser Bundle</a> and access this content over Tor.</div>
+        <div>Please send us your <a href="javascript:show_hide_notification_form()">feedback</a> and if you have concerns with this content, send us an <a href="javascript:show_hide_notification_form()">abuse notice</a>.</div>
+        <div>For more informations please refer to <a href="/antanistaticmap/tos.html">Tor2Web Terms of Services.</a></div>
+        <div><t:transparent t:render="mirror" /></div>
+      </div>
+      <div id="tor2web_notification_form">
+        <fieldset>
+          <legend>Notification:</legend>
+          BY:
+         <div><input type="text" id="by" name="by" /></div>
+         URL:
+         <div><input type="text" id="url" name="url" /></div>
+         COMMENT:
+         <div><textarea type="text" id="comment" name="comment" rows="10" cols="20"></textarea></div>
+         <div><input type="button" value="Send" onclick="sendNotification()" /></div>
+        </fieldset>
+      </div>
+      <div style="clear:both"></div>
+      <div class="tor2web_showhide">
+        <a href="javascript:show_hide_tor2web_header(true)">hide Tor2web header</a>
+      </div>
+    </div>
+    <div id="tor2web-hidden">
+      <div class="tor2web_showhide">
+        <a href="javascript:show_hide_tor2web_header(false)">show Tor2web header</a>
+      </div>
+    </div>
+  </div>
+</div>

tor2web/utils/socks.py
@@ -84,12 +84,11 @@ class SOCKSv5ClientProtocol(ProtocolWrapper):
 
         self._buf = self._buf[2:]
 
-        self.state = 2 
-
         if not self._optimistic:
             self.transport.write(struct.pack("!BBBBB", 5, 1, 0, 3, len(self._host)) + self._host + struct.pack("!H", self._port))
-        else:
-            self.socks_state_2()
+
+        self.state = 2
+        getattr(self, 'socks_state_%s' % self.state)()
 
     def socks_state_2(self):
         if len(self._buf) < 2:
@@ -102,6 +101,7 @@ class SOCKSv5ClientProtocol(ProtocolWrapper):
         self._buf = self._buf[2:]
 
         self.state = 3
+        getattr(self, 'socks_state_%s' % self.state)()
 
     def socks_state_3(self):
         if len(self._buf) < 8:

commit 2e07ea74230376abca61b4a6961e097c792d6804

fixed race condition introduced in commit https://github.com/globaleaks/Tor2web-3.0/commit/8f1c1b504dc4df354ae703e0df603177d42af706#diff-a47f20a06d5e5ae3474b4c40e29271fbR816

tor2web/t2w.py
@@ -399,6 +399,11 @@ class T2WRequest(http.Request):
 
         self.pool = pool
 
+        self.rexp = {
+            'head': re.compile(r'(<head.*?\s*>)', re.I)
+        }
+
+
     def _cleanup(self):
         """
         Method overridden to avoid self.content actions.
@@ -466,12 +471,12 @@ class T2WRequest(http.Request):
         data = self.stream + data
 
         if len(data) >= 1000:
-            data = re_sub(rexp['t2w_from'], rexp['t2w_to'], data)
+            data = re_sub(self.rexp['t2w_from'], self.rexp['t2w_to'], data)
 
             forward = data[:-500]
             if not self.header_injected and forward.find("<head") != -1:
                 banner = yield flattenString(self, templates['banner.tpl'])
-                forward = re.sub(rexp['head'], partial(self.add_banner, banner), forward)
+                forward = re.sub(self.rexp['head'], partial(self.add_banner, banner), forward)
                 self.header_injected = True
 
             self.forwardData(self.handleCleartextForwardPart(forward))
@@ -488,11 +493,11 @@ class T2WRequest(http.Request):
 
         data = self.stream + data
 
-        data = re_sub(rexp['t2w_from'], rexp['t2w_to'], data)
+        data = re_sub(self.rexp['t2w_from'], self.rexp['t2w_to'], data)
 
         if not self.header_injected and data.find("<head") != -1:
             banner = yield flattenString(self, templates['banner.tpl'])
-            data = re.sub(rexp['head'], partial(self.add_banner, banner), data)
+            data = re.sub(self.rexp['head'], partial(self.add_banner, banner), data)
             self.header_injected = True
 
         data = self.handleCleartextForwardPart(data, True)
@@ -636,7 +641,7 @@ class T2WRequest(http.Request):
         for key, values in self.obj.headers.getAllRawHeaders():
             fixed_values = []
             for value in values:
-                value = re_sub(rexp['w2t_from'], rexp['w2t_to'], value)
+                value = re_sub(self.rexp['w2t_from'], self.rexp['w2t_to'], value)
                 fixed_values.append(value)
 
             self.obj.headers.setRawHeaders(key, fixed_values)
@@ -833,10 +838,10 @@ class T2WRequest(http.Request):
                     self.sendError(403)
                     defer.returnValue(NOT_DONE_YET)
 
-            rexp['w2t_from'] = re.compile(r'(http.?:)?//' + request.host + '(?!:\d+)', re.I)
-            rexp['w2t_to'] = r'http://' + self.obj.onion
-            rexp['t2w_from'] = re.compile(r'(http.?:)?//([a-z0-9]{16}).onion(?!:\d+)', re.I)
-            rexp['t2w_to'] = r'https://' + request.host
+            self.rexp['w2t_from'] = re.compile(r'(http.?:)?//' + request.host + '(?!:\d+)', re.I)
+            self.rexp['w2t_to'] = r'http://' + self.obj.onion
+            self.rexp['t2w_from'] = re.compile(r'(http.?:)?//([a-z0-9]{16}).onion(?!:\d+)', re.I)
+            self.rexp['t2w_to'] = r'https://' + request.host
 
             self.process_request(request)
 
@@ -942,7 +947,7 @@ class T2WRequest(http.Request):
         if keyLower in 'location':
             fixed_values = []
             for value in values:
-                value = re_sub(rexp['t2w_from'], rexp['t2w_to'], value)
+                value = re_sub(self.rexp['t2w_from'], self.rexp['t2w_to'], value)
                 fixed_values.append(value)
             values = fixed_values
 
@@ -1094,15 +1099,10 @@ def start_worker():
     global antanistaticmap
     global templates
     global pool
-    global rexp
     global ports
 
     lc = LoopingCall(updateListsTask)
     lc.start(600)
-
-    rexp = {
-        'head': re.compile(r'(<head.*?\s*>)', re.I)
-    }
 
     ###############################################################################
     # Static Data loading