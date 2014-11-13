da4c3a0b3fad06f09251896fe84cae6bb99973eb
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2014-11-13 13:21:29 +0100

    fixed header injection twice bug

--- data/static/tor2web.js
+++ data/static/tor2web.js
@@ -68,51 +68,12 @@ function show_hide_notification_form() {
 var tor2web_notification_form_visible = true;
 var http = getHTTPObject();
 
-var banner = '' +
-'<style type="text/css">@import url(/antanistaticmap/tor2web.css);</style>' +
-'<div id="tor2web-banner">' +
-'  <div id="tor2web-visible">' +
-'    <div id="tor2web_logo">' +
-'      <a href="https://www.tor2web.org"><img src="/antanistaticmap/tor2web-small.png" alt="tor2web logo" /></a>' +
-'    </div>' +
-'    <div id="tor2web_disclaimer">' +
-'      <div><b>tor2web.org does not host this content</b>; the service is simply a proxy connecting Internet users to content hosted inside the <a href="https://www.torproject.org/docs/hidden-services.html.en">Tor network.</a></div>' +
-'      <div>Please be aware that when you access this site through a Tor2web proxy you are not anonymous. To obtain anonymity, you are strongly advised to <a href="https://www.torproject.org/download/">download the Tor Browser Bundle</a> and access this content over Tor.</div>' +
-'      <div>Please send us your <a href="javascript:show_hide_notification_form()">feedback</a> and if you have concerns with this content, send us an <a href="javascript:show_hide_notification_form()">abuse notice</a>.</div>' +
-'      <div><t:transparent t:render="mirror" /></div>' +
-'    </div>' +
-'    <div id="tor2web_notification_form">' +
-'      <fieldset>' +
-'        <legend>Notification:</legend>' +
-'        BY:' +
-'        <div><input type="text" id="by" name="by" /></div>' +
-'        URL:' +
-'        <div><input type="text" id="url" name="url" readonly="readonly" /></div>' +
-'        COMMENT:' +
-'        <div><textarea type="text" id="comment" name="comment" rows="10" cols="20"></textarea></div>' +
-'        <div><input type="button" value="Send" onclick="sendNotification()" /></div>' +
-'      </fieldset>' +
-'    </div>' +
-'    <div style="clear:both"></div>' +
-'    <div class="tor2web_showhide">' +
-'      <a href="javascript:show_hide_tor2web_header(true)">hide Tor2web header</a>' +
-'    </div>' +
-'  </div>' +
-'  <div id="tor2web-hidden">' +
-'    <div class="tor2web_showhide">' +
-'      <a href="javascript:show_hide_tor2web_header(false)">show Tor2web header</a>' +
-'    </div>' +
-'  </div>' +
-'</div>';
-
 window.onload = function() {
   if(document.getElementById('tor2web') != null) {
 
     document.getElementById('url').value = document.location;
 
   } else {
-
-    document.body.innerHTML = banner + "<div style='position:relative'>" + document.body.innerHTML + "</div>";
 
     if(jaaulde.utils.cookies.get('tor2web_header_hidden') && jaaulde.utils.cookies.get('tor2web_header_hidden') == true) {
       show_hide_tor2web_header(true);
