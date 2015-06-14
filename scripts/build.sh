c82da6b50ab524e0f223b86c7daa6401ce78eb45
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-06-14 21:52:29 +0200

    Make possible to build a precise tag/branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,24 +2,34 @@
 
 set -e
 
-die() {
-  echo "Please specify a valid distro codename"
-  echo "Available: precise, trusty, wheezy, jessie";
-  echo "e.g.: $0 precise"
-  exit 1;
+usage() {
+  echo "Tor2web Build Script"
+  echo "Valid options:"
+  echo " -h"
+  echo -e " -t tagname (build specific release/branch)"
+  echo -e " -d distribution (available: precise, trusty, wheezy, jessie)"
+  echo -e " -n (do not sign)"
+  
 }
 
 DISTRIBUTION="precise"
+TAG="master"
 NOSIGN=0
 
-while getopts "d:n" opt; do
+while getopts "d:n:th" opt; do
   case $opt in
     d) DISTRIBUTION="$OPTARG"
     ;;
+    t) TAG="$OPTARG"
+    ;;
     n) NOSIGN=1
     ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-        die
+    h)
+        usage
+        exit 1
+    ;;
+    \?) usage
+        exit 1
     ;;
   esac
 done
@@ -28,7 +38,8 @@ if [ "$DISTRIBUTION" != "precise" ] &&
    [ "$DISTRIBUTION" != "trusty" ] &&
    [ "$DISTRIBUTION" != "wheezy" ] &&
    [ "$DISTRIBUTION" != "jessie" ]; then
- die
+  usage
+  exit 1 
 fi
 
 echo "Packaging Tor2web for:" $DISTRIBUTION
@@ -39,6 +50,7 @@ mkdir T2WRelease
 cd T2WRelease
 git clone git@github.com:globaleaks/Tor2web.git
 cd Tor2web
+git checkout $TAG
 sed -i "s/stable; urgency=/$DISTRIBUTION; urgency=/g" debian/changelog
 
 if [ $NOSIGN -eq 1 ]; then
