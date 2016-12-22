eba4fc949166bd5bb45b195e5116dec3612e316c
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-12-22 09:29:38 +0100

    Add package build to travis CI

---
+++ scripts/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get update -y
+
+sudo apt-get install -y debhelper devscripts dh-apparmor dh-python python python-pip python-setuptools python-sphinx
+
+pip install -r requirements.txt
+pip install coverage coveralls
+
+coverage run setup.py test # tests still to be implemented
+coveralls
+
+./scripts/build.sh -d trusty -t $TRAVIS_COMMIT -n
