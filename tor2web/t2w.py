ee571cc69db2d5de6611577b5e27b957b482779d
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-01-16 18:35:10 +0100

    Fix writeContent file-size calculation

--- tor2web/t2w.py
+++ tor2web/t2w.py
@@ -608,14 +608,15 @@ class T2WRequest(http.Request):
     def writeContent(self, data):
         self.setHeaders()
 
-        self.setHeader(b'content-length', intToBytes(len(data)))
+         if len(data):
+             if self.obj.client_supports_gzip:
+                 self.setHeader(b'content-encoding', b'gzip')
+                 data = self.zip(data, True)
 
-        if len(data):
-            if self.obj.client_supports_gzip:
-                self.setHeader(b'content-encoding', b'gzip')
-                data = self.zip(data, True)
-
-            self.write(data)
+             self.setHeader(b'content-length', intToBytes(len(data)))
+             self.write(data)
+         else:
+             self.setHeader(b'content-length', intToBytes(0))
 
         self.finish()
 
